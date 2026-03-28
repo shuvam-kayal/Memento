c9321ffde04bc3439198439702ce3cea17c73bed
shuvam-kayal <shuvam.ky@gmail.com>
2026-03-29 03:47:57 +0530

    Add FPP old lady controller and block out house collision

---
+++ Scenes/Characters/OldLady/old_lady_controller.gd
@@ -0,0 +1,45 @@
+extends CharacterBody3D
+
+const SPEED = 3.0
+const JUMP_VELOCITY = 3.0
+const MOUSE_SENSITIVITY = 0.002
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# We reference the Head node to look up and down
+@onready var head = $Head
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		# Rotate the whole body left and right (Changes walking direction)
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		
+		# Rotate ONLY the head up and down
+		head.rotate_x(event.relative.y * MOUSE_SENSITIVITY)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Now using your newly mapped WASD keys
+	var input_dir = Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
