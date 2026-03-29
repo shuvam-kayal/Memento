df44f4263cfb849873525fd59e917ff009829e6c
SriVineel <srivineel39@gmail.com>
2026-03-29 17:40:59 +0530

    Completed memory 0

---
+++ Assets/doorhandle/doorknob.gd
@@ -0,0 +1,12 @@
+extends StaticBody3D
+
+const LIVING_ROOM_SCENE = "res://Scenes/Environments/LivingRoom/LivingRoom.tscn"
+
+func _ready() -> void:
+	pass
+
+func interact():
+	get_tree().change_scene_to_file(LIVING_ROOM_SCENE)
+
+func _process(_delta: float) -> void:
+	pass
