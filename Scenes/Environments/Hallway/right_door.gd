c595ec29cccb51c09e1c7092f4d9175cdcebbe94
Aditya <adityaarvindsingh2006@gmail.com>
2026-03-30 00:45:27 +0530

    Hallway FIxed

--- Scenes/Environments/Hallway/right_door.gd
+++ Scenes/Environments/Hallway/right_door.gd
@@ -1,4 +1,7 @@
-extends StaticBody3D
-
-func interact():
-	get_node("/root/Memory1").enter_birthday_room()
+extends Node
+func enter_birthday_room():
+	# 1. Define the path to the scene you just saved in Step 1
+	var next_scene_path = "res://Scenes/Environments/House/birthday_room.tscn"
+	
+	# 2. Tell the game tree to load and switch to that file
+	get_tree().change_scene_to_file(next_scene_path)
